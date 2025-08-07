bc7b074e2f1fa3e936053edf984e93cdbc57658e
Abdul Kader Anik <153476736+anik071@users.noreply.github.com>
2025-08-07 21:48:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+let i=0;
+let grid=[[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+let ok=0,flag=0;
+document.getElementById("trn").innerHTML = "O's turn";
+function play(row,column,id){
+    if(document.getElementById(id).innerHTML != ""){
+        alert("This box is already filled. Please choose another box.");
+        return;
+    }
+    if(ok){
+        alert("The Game is Over Already Please Restart !!");
+        return;
+    }
+    i++;
+    if(i%2==1){
+        document.getElementById("trn").innerHTML = "O's turn";
+        document.getElementById(id).innerHTML = "O";
+        grid[row][column]=0;
+        console.log(grid);
+
+}else{
+        document.getElementById("trn").innerHTML = "X's turn";
+        document.getElementById(id).innerHTML = "X";
+        grid[row][column]=1;
+        console.log(grid);
+    }
+    if(i>4){
+        for (let j = 0; j < 3; j++) {
+            if(grid[j][0]!=-1 && grid[j][0]==grid[j][1] && grid[j][2]==grid[j][1]){
+                ok=1;
+                if(grid[j][0]==0){
+                document.getElementById("trn").innerHTML = "O wins   Game Over!!!";
+                console.log(grid);
+                 reset();
+                console.log(grid);
+                return;}
+                else{
+                    document.getElementById("trn").innerHTML = "X wins   Game Over!!!";
+                    console.log(grid);
+                    reset();
+                    console.log(grid);
+                return;
+                }
+            }          
+        }
+        for (let j = 0; j < 3; j++) {
+            if(grid[j][0]!=-1 && grid[0][j]==grid[1][j] && grid[1][j]==grid[2][j]){
+                ok=1;
+                if(grid[0][j]==0){
+                document.getElementById("trn").innerHTML = "O wins   Game Over!!!";
+                console.log(grid);
+                reset();
+                console.log(grid);
+                return;
+            }else{
+                    document.getElementById("trn").innerHTML = "X wins   Game Over!!!";
+                    console.log(grid);
+                    reset();
+                    console.log(grid);
+                return;
+                }
+            }          
+        }
+        if(grid[0][0]==grid[1][1] && grid[1][1]==grid[2][2] && grid[0][0]!=-1){
+            ok=1;
+                if(grid[0][0]==0){
+                document.getElementById("trn").innerHTML = "O wins   Game Over!!!";
+                console.log(grid);
+                reset();
+                console.log(grid);
+                return;
+            }else{
+                    document.getElementById("trn").innerHTML = "X wins   Game Over!!!";
+                    console.log(grid);
+                    reset();
+                    console.log(grid);
+                return;
+                }
+        }
+        if(grid[0][2]==grid[1][1] && grid[1][1]==grid[2][0] && grid[0][2]!=-1){
+            ok=1;
+                if(grid[1][1]==0){
+                document.getElementById("trn").innerHTML = "O wins   Game Over!!!";
+                console.log(grid);
+                reset();
+                console.log(grid);
+                return;
+            }else{
+                    document.getElementById("trn").innerHTML = "X wins   Game Over!!!";
+                    console.log(grid);
+                    reset();
+                    console.log(grid);
+                return;
+                }
+        }
+
+    }
+    if(i==9){
+        document.getElementById("trn").innerHTML = "DRAW   Game Over!!!";
+        reset();
+        return;
+    }
+}
+function reset(){
+    grid=[[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+}
+function restart(){
+    i=0;
+    grid=[[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+    ok=0;
+    document.getElementById("trn").innerHTML = "O's turn";
+    let headings = document.getElementsByTagName('h2');
+for (let i = 0; i < headings.length; i++) {
+  headings[i].innerHTML = "";
+}
+}
